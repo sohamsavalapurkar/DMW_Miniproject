dfbb0e388275c7e11dc6f232108f7ac5dffdc28f
soham <soham1999@gmail.com>
2020-10-22 10:45:42 +0530

    Update with kind of equal spacing columns

--- frontend.py
+++ frontend.py
@@ -16,27 +16,37 @@ def getTestInput():
         text.insert(tkinter.END, filename)
 
 print_output = {1:'positive', 2: 'negative'}        
-        
+labels = []  
 def returnTestInput():
+    
     if v.get() == 1:
         accuracy, confusion_matrix = backend_class.read_test_data_and_predict(text.get())
-        Label(window, text='Naive Bayes').grid(row=3,column=1)
-        Label(window, text=f'Accuracy : {accuracy[0]}%').grid(row=4,column=1 )
-        Label(window, text=f'Confusion Matrix : ').grid(row=5,column=1 )
-        Label(window, text=f'{confusion_matrix[0][0]}').grid(row=6,column=1 )
-        Label(window, text=f'{confusion_matrix[0][1]}').grid(row=7,column=1 )
+        tmp = Label(window, text='Naive Bayes').grid(row=3,column=1)
+        labels.append(tmp)
+        tmp = Label(window, text=f'Accuracy : {accuracy[0]}%').grid(row=4,column=1 )
+        labels.append(tmp)
+        tmp = Label(window, text=f'Confusion Matrix : ').grid(row=5,column=1 )
+        labels.append(tmp)
+        tmp = Label(window, text=f'{confusion_matrix[0][0]}').grid(row=6,column=1 )
+        labels.append(tmp)
+        tmp = Label(window, text=f'{confusion_matrix[0][1]}').grid(row=7,column=1 )
+        labels.append(tmp)
 
-        Label(window, text='KNN').grid(row=3,column=7)
-        Label(window, text=f'Accuracy : {accuracy[1]}%').grid(row=4,column=7)
-        Label(window, text=f'Confusion Matrix : ').grid(row=5,column=7 )
-        Label(window, text=f'{confusion_matrix[1][0]}').grid(row=6,column=7 )
-        Label(window, text=f'{confusion_matrix[1][1]}').grid(row=7,column=7 )
+        tmp = Label(window, text='').grid(column=2)
+
+        Label(window, text='KNN').grid(row=3,column=3)
+        Label(window, text=f'Accuracy : {accuracy[1]}%').grid(row=4,column=3)
+        Label(window, text=f'Confusion Matrix : ').grid(row=5,column=3)
+        Label(window, text=f'{confusion_matrix[1][0]}').grid(row=6,column=3 )
+        Label(window, text=f'{confusion_matrix[1][1]}').grid(row=7,column=3 )
     
-        Label(window, text='Random Forest').grid(row=3,column=10)
-        Label(window, text=f'Accuracy : {accuracy[2]}%').grid(row=4,column=10)
-        Label(window, text=f'Confusion Matrix : ').grid(row=5,column=10)
-        Label(window, text=f'{confusion_matrix[2][0]}').grid(row=6,column=10 )
-        Label(window, text=f'{confusion_matrix[2][1]}').grid(row=7,column=10 )
+        Label(window, text='').grid(column=4)
+
+        Label(window, text='Random Forest').grid(row=3,column=5)
+        Label(window, text=f'Accuracy : {accuracy[2]}%').grid(row=4,column=5)
+        Label(window, text=f'Confusion Matrix : ').grid(row=5,column=5)
+        Label(window, text=f'{confusion_matrix[2][0]}').grid(row=6,column=5 )
+        Label(window, text=f'{confusion_matrix[2][1]}').grid(row=7,column=5 )
 
     else:
         output = backend_class.read_test_text_and_predict(text.get())
@@ -49,6 +59,7 @@ def returnTestInput():
     return (v.get(), text.get())
 
 
+
 v = IntVar() 
 Radiobutton(window, text='File', variable=v, value=1, command=getTestInput).grid(row=1)
 Radiobutton(window, text='Text', variable=v, value=2, command=getTestInput).grid(row=1,column=1)
@@ -57,7 +68,16 @@ text.grid(row=1, column=9)
 Label(window, text='').grid(row=2)
 Label(window, text='Filepath/Text').grid(row=1,column=8 )
 load_data = Button(window, text = 'Load Data',command=returnTestInput).grid(row=1,column=11) 
-naive_bayes = Text(window, height=10, width=20)
 
+
+
+col_count, row_count = window.grid_size()
+print(col_count)
+print(row_count)
+for col in range(col_count):
+    window.grid_columnconfigure(col, minsize=20)
+
+for row in range(row_count):
+    window.grid_rowconfigure(row, minsize=20)
 
 window.mainloop()
